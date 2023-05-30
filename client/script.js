405098a86dd35dcb5a382ebbc6607c581dee964a
Linh Phan <phanthuylinh221203@gmail.com>
2023-05-30 23:43:13 +0700

    update function

---
+++ client/script.js
@@ -0,0 +1,13 @@
+//import icon from assets
+import bot from './assets/bot.png';
+import user from './assets/user.png';
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('chat_container');
+
+let loadInterval;
+
+//loader: clear content of element and set interval function that adds a dot to text content every 300 milliseconds
+function loader(element) { //element is HTML
+
+}
